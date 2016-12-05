8dd61b6e56d803baee2aa3d48ed1b448b615fddf
vincentyao <75587880@qq.com>
2016-12-05 18:08:00 +0800

    a proxy ip get python file

---
+++ ipGet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.5
+# -*- coding:utf8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+
+init_url='get proxy_ip messages website'
+
+headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"}
+
+init_html=requests.get(init_url,headers=headers)
+init_soup=BeautifulSoup(init_html.text,'lxml')
+
+result = init_soup.find_all('ul',class_='l2')
+
+for ipSrc in result:
+    ipSrc_r=ipSrc.find('span').find('li').extract()
+    print(ipSrc_r)
