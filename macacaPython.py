e9dd0284cd9497f3d11391c9ce822b7c1e8eb9f2
vincentyao <75587880@qq.com>
2016-12-05 18:20:40 +0800

    first

--- macacaPython.py
+++ macacaPython.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python3.5
 #-*- coding:utf8 -*-
 
-from macaca import WebDriver, WebElement
-
-# Configure the desired capabilities.
-desired_caps = {
-    'autoAcceptAlerts': True,
-    'browserName': 'electron',
-    'platformName': 'desktop'
-}
-
-driver = WebDriver(desired_caps)
-
-# Start the WebDriver session
-driver.init()
-
-# Support fluent API
-driver.set_window_size(1280, 800).get("http://www.baidu.com/")
-
-# Get WebElement instance through element_by_* APIs.
-web_element = driver.element_by_id("kw")
-web_element.send_keys('macaca')
+import requests
+url = 'http://www.baidu.com'
+r = requests.get(url)
+print (r.text)
